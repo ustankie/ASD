dbe0d250b5580ad98ced160934723b4c60a5f6ce
urszula <ustankiewicz@student.agh.edu.pl>
2023-06-23 21:43:11 +0200

    Send version of zad9

---
+++ lab12/lab.py
@@ -0,0 +1,58 @@
+#1) problem stacji benzynowych - traktor jedzie z A do B, w baku dokładnie L litrów paliwa - ograniczenie na bak!
+#   a) minimalna liczba tankowań - bierzemy ostatnia napotkana stacje i na niej tankujemy
+#   b)minimalizujemy koszt tankowań - jeśli w zasięgu lepsza stacja, to tankujemy tyle, ile trzeba, by do niej dojechać, wpp do pełna
+#   c)j.w.,ale tankujemy zawsze do pełna - dynamik, a nie zachłanny f(x,i)-koszt jazdy z 0 do i mając x paliwa
+#2) przedziały jednostkowe - sortujemy pkty rosnąco zaczynamy na pierwszym do pokrycia punkcie jednostkowym, kolejny zaczynamy od pierwszego poza przedziałem
+#3) zadania z terminami - zadanie,termin,zysk, każde zadanie zajmuje jednostkę czasu - chcemy znaleźć podzbiór zadań, które można wykonać w terminie i max zysk
+#   - bierzemy zadania o największym zysku idąc od najpóźniejszych deadlinów z tych które można jeszcze oddać
+#4) jak najmniej monet wydać w reszcie - dynamik: f(N,coins)=min(N-c,coins)+1,c należy do listy nominałów
+#                                                 f(0,_)=0
+#   czasem można zachłannie -> np w pl systemie monetarnym -> nominały muszą rosnąć szybciej niż wykładniczo - kolejny jest co najmniej 2 razy większy
+#5)ładowanie przyczepy - posortować i dodawać od największych
+
+#1) a)
+def tank(L,S):
+    n=len(S)
+    tankowania=0
+    stations=[]
+    pos=0
+    while pos<n:
+        i=pos+L
+        while i>=pos and S[i]==None:
+            i-=1
+        tankowania+=1
+        stations.append(pos)
+        pos=i
+#1) b) O(nllogl)
+def tankb(L,S):
+    pos=0
+    n=len(S)
+    cheapest=0
+    paliwo=0
+    koszt=0
+    while pos<n:
+        cheapest=sorted(list([x for x in enumerate(S)[pos:pos+L] if x[1]!=None]),key=lambda x:x[1])[0]
+        if cheapest[0]==pos:
+            brakuje=min(L-paliwo,n-pos-paliwo)
+            koszt+=brakuje*cheapest[1]
+            paliwo+=brakuje
+            pos=min(pos+L,n-1)
+        else:
+            brakuje=max((cheapest[0]-pos)-paliwo,0)
+            paliwo+=brakuje
+            koszt+=brakuje*S[pos]
+            pos+=(cheapest[0]-pos)
+def jedn(T):
+    n=len(T)
+    T.sort(reversed=True)
+    licznik=0
+    while T:
+        start=T[-1]
+        while start<=T[-1]<=start+1:
+            T.pop()
+        licznik+=1
+    return licznik
+
+        
+
+
--- offline/zad9/send/zad9.py
+++ offline/zad9/send/zad9.py
@@ -3,6 +3,7 @@
 #F[i][1] - minimalny koszt dotarcia do pola i wykorzystawszy skok 2*T
 #F[i][0]=min(F[j][0])+C[i], 0<=j<i, 0<=O[i]-O[j]<=T
 #F[i][1]=min(F[j][0])+C[i], 0<=j<i, 0<=O[i]-O[j]<=2*T
+#Najpierw sortujemy tablice O,C wg rosnących odległości parkingów, a potem w podwójnej pętli for uzupełniamy tablicę.
 #Algorytm ma złożoność O(n^2)
 
 from zad9testy import runtests
@@ -79,44 +80,7 @@ def min_cost( O, C, T, L ):
 
     return min(F[n-1])
 
-def  min_cost2( O, C, T, L ):
-    O,C=(merge_sort(O,C,L))
-    O.append(L)
-    C.append(0)
-    O=[0]+O
-    C=[0]+C
-
-    n=len(O)
-
-    F=[[float('inf'),float('inf')]for _ in range(n)]
-    F[0][0]=0
-    F[0][1]=0
-    def rec(i,k):
-        if i==0:
-            return 0
-        if F[i][k]<float('inf'):
-            return F[i][k]
-        j=i-1
-        while j>=0 and O[i]-O[j]<=T:
-            if F[j][k]==float('inf'):
-                F[j][k]=rec(j,k)
-            F[i][k]=min(F[i][k],F[j][k]+C[i])
-            j-=1
-        if k==1:
-            while j>=0 and O[i]-O[j]<=(2*T):
-                if F[j][0]==float('inf'):
-                    F[j][0]=rec(j,0)
-                F[i][1]=min(F[i][1],F[j][0]+C[i])
-                j-=1
-        return F[i][k]
-    return min(rec(n-1,0),rec(n-1,1))
         
 
 runtests( min_cost, all_tests = True )
 
-O = [17, 20, 11, 5, 12]
-C = [9, 7, 7, 7, 3]
-T = 7
-L = 25
-
-print(min_cost(O,C,T,L))
